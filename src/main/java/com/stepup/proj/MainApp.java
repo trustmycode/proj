41d4745b0e40765df8f976355cb22d5371dc9bac
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-04-30 12:50:19 +0300

    улучшена логика работы

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -2,11 +2,14 @@ package com.stepup.proj;
 
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println("----");
         System.out.println("Hello World");
         System.out.println("111111");
     }
 
     public static void did() {
+        System.out.println(1);
+        System.out.println(1);
         System.out.println(1);
     }
 }
